1ee9a5e7b9d59c8d866cc6628cb15883f460fdce
DILHAQUE <95963252+itsdlhqcp@users.noreply.github.com>
2024-05-08 09:56:46 +0530

    Update response.js
    verify removed

--- routes/response.js
+++ routes/response.js
@@ -6,7 +6,7 @@ const Response = require('../models/Response');
 
 
 //CREATE
-router.post("/create",verifyToken,async(req,res)=>{
+router.post("/create",async(req,res)=>{
   try{
      const newResponse=new Response(req.body)
      const savedResponse=await newResponse.save()
@@ -19,7 +19,7 @@ router.post("/create",verifyToken,async(req,res)=>{
 
 
 //UPDATE
-router.put("/:id",verifyToken,async (req,res)=>{
+router.put("/:id",async (req,res)=>{
     try{
        
         const updatedResponse=await Response.findByIdAndUpdate(req.params.id,{$set:req.body},{new:true})
@@ -58,7 +58,7 @@ router.get("/:ottId",async (req,res)=>{
 
 
 //GET OTT RESPONSE
-router.get("/ott/:ottId",verifyToken,async (req,res)=>{
+router.get("/ott/:ottId",async (req,res)=>{
     try{
         const Responses=await Response.find({ottId:req.params.ottId})
         res.status(200).json(Responses)
