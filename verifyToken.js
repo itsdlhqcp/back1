0141ab469ee6d8a94479be8304e35e4431e97a7f
DILHAQUE <95963252+itsdlhqcp@users.noreply.github.com>
2024-05-08 10:12:25 +0530

    Update verifyToken.js
    verify token error handling

--- verifyToken.js
+++ verifyToken.js
@@ -1,22 +1,27 @@
-const jwt=require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
-const verifyToken=(req,res,next)=>{
-    const token=req.cookies.token
-    // console.log(token)
-    if(!token){
-        return res.status(401).json("You are not authenticated!")
+const verifyToken = (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(401).json("You are not authenticated!");
     }
-    jwt.verify(token,process.env.SECRET,async (err,data)=>{
-        if(err){
-            return res.status(403).json("Token is not valid!")
+    
+    jwt.verify(token, process.env.SECRET, async (err, data) => {
+        if (err) {
+            return res.status(403).json("Token is not valid!");
+        }
+
+        // Ensure that data contains the expected properties
+        if (!data || !data._id) {
+            return res.status(403).json("Invalid token data!");
         }
         
-        req.userId=data._id
-       
-       // console.log("passed")
-        
-        next()
-    })
-}
+        // Assign the user ID from the token to req.userId
+        req.userId = data._id;
 
-module.exports=verifyToken
+        // Proceed to the next middleware or route handler
+        next();
+    });
+};
+
+module.exports = verifyToken;
